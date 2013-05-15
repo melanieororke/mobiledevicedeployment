983303d262c4d7ded2e1d4739143c4e56f70951b
melanieororke <melanieororke@fullsail.edu>
2013-05-15 16:38:45 -0500

    Visual Framework Utilizing Style Guide
    Static index page, non-responsive.

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Easy Keepr | Your Stable Management App</title>
+    <meta name="description" content="Easy Keepr is the premiere stable management application. Easy Keepr boasts of features like 
+								easy updates to your stabled horses, feed and rations schedule, show schedule and even health records. Easy Keepr is 
+								made to make the busy stable manager's life easier." />
+    <meta name="author" content="Melanie O'Rorke">
+
+    <!-- Le styles -->
+		<link href="http://melanieororkedesign.com/easykeepr/assets/css/overwrites.css" rel="stylesheet">
+    <link href="http://melanieororkedesign.com/easykeepr/assets/css/bootstrap.css" rel="stylesheet">
+		<link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://melanieororkedesign.com/easykeepr/assets/js/html5shiv.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+		<div class="container containerbg" style="padding: 20px;"> <!-- container -->
+			<div class="row"><!-- Begin header row -->
+				<div class="span3"><!--Begin Logo Column-->
+					<a href="index.php"><img src="http://melanieororkedesign.com/easykeepr/assets/img/logo.png"></a>
+				</div><!--End Logo Column-->
+				<div class="span9 navbar"><!--Begin Nav Column-->
+					<ul class="nav" style="padding-top: 10px; padding-right: 50px;">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="horses.php">Horses</a></li>
+						<li><a href="feed.php">Feed Schedules</a></li>
+						<li><a href="feed.php">Health Records</a></li>
+						<li><a href="show.php">Show Schedules</a></li>
+						<li><a href="account.php">My Account</a></li>
+						<li>
+					</ul>
+				</div><!--End Nav Column-->
+			</div><!-- End header row-->
+			<div class="row"><!--Row for HR-->
+				<div class="span12"><hr></div>
+			</div><!-- /row for HR -->
+			<div class="row"><!--Row for Main Headline-->
+				<div class="span12">
+					<h1 class="pink" align="center">Stable Management at Your Fingertips</h1>
+				</div>
+			</div><!-- /main headline-->
+			<div class="row"><!--Row for HR-->
+				<div class="span12"><hr></div>
+			</div><!-- /row for HR -->
+			<div class="row"><!-- Row for Features-->
+				<div class="span3"  align="center"><!-- column one-->
+						<div class="circle"><i class="icon-info whiteIcon iconPosit"></i></div>
+						<h4 class="green">Horse Information</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. </p>
+				</div><!-- / column one-->
+				<div class="span3"  align="center"><!-- column two-->
+						<div class="circle"><i class="icon-leaf whiteIcon iconPosit"></i></div>
+						<h4 class="green">Feed Schedules</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. </p>
+				</div><!-- / column two-->
+				<div class="span3"  align="center"><!-- column three-->
+						<div class="circle"><i class="icon-medkit whiteIcon iconPosit"></i></div>
+						<h4 class="green">Health Records</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. </p>
+				</div><!-- / column three-->
+				<div class="span3"  align="center"><!-- column four-->
+						<div class="circle"><i class="icon-calendar whiteIcon iconPosit"></i></div>
+						<h4 class="green">Show Schedules</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. </p>
+				</div><!-- / column four-->
+			</div><!--/row for features-->
+			<div class="row"><!--Row for HR-->
+				<div class="span12"><hr></div>
+			</div><!-- /row for HR -->
+			<div class="row"><!--Row for intro-->
+				<div class="span4">
+					<h2 class="pink">Headline</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. Praesent tempor molestie libero in congue. Morbi nec justo ut libero ornare dignissim a ut erat. Vestibulum malesuada sollicitudin magna non vehicula. Nunc pharetra magna nec odio iaculis pharetra. Nam dignissim lorem lobortis sapien molestie molestie. Duis tempor venenatis dui a varius.</p>
+				</div>
+				<div class="span4">
+					<h2 class="pink">Headline</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. Praesent tempor molestie libero in congue. Morbi nec justo ut libero ornare dignissim a ut erat. Vestibulum malesuada sollicitudin magna non vehicula. Nunc pharetra magna nec odio iaculis pharetra. Nam dignissim lorem lobortis sapien molestie molestie. Duis tempor venenatis dui a varius.</p>
+				</div>
+				<div class="span4">
+					<form class="form-signin">
+						<h5 class="form-signin-heading pink">Returning Members? Sign in here!</h5>
+						<input type="text" class="input-block-level" placeholder="Username">
+						<input type="text" class="input-block-level" placeholder="Password">
+						<p class="reset pink" align="right">Not a Member? <a href="password.php">Sign up now!</a> <br/>
+						<a href="password.php">Reset Password?</a></p>
+						<button class="btn btn-primary" type="submit">Sign In</button>
+					</form>
+				</div>
+			</div><!--/row for intro-->
+
+    </div> <!-- /container -->
+		<div class="container"><!--Container for footer-->
+			<div class="row">
+				<div class="span12">
+					<p class="copyright">Copyright &copy; 2013 Melanie O'Rorke | Designed and Developed for Mobile Device Deployment Course at Full Sail University<br/>
+						Easy Keepr is a fictitious site intended for educational purposes only.</p>
+				</div>
+			</div>
+		</div>
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/jquery.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-transition.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-alert.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-modal.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-dropdown.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-tab.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-tooltip.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-popover.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-button.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-collapse.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-carousel.js"></script>
+    <script src="http://melanieororkedesign.com/easykeepr/assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>

a95bac2f0345c63ef3fc2c969fe896d311fa0e45
melanieororke <melanieororke@fullsail.edu>
2013-05-23 17:51:09 -0500

    Adjusting Button Name
    Submit button said "Register" instead of "Sign In"

--- index.php
+++ index.php
@@ -42,25 +42,81 @@
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. Praesent tempor molestie libero in congue. Morbi nec justo ut libero ornare dignissim a ut erat. Vestibulum malesuada sollicitudin magna non vehicula. Nunc pharetra magna nec odio iaculis pharetra. Nam dignissim lorem lobortis sapien molestie molestie. Duis tempor venenatis dui a varius.</p>
 				</div>
 				<div class="span4">
-					<?php 
+					<!--Begin Login Form--><?php
 
-					include('includes/functions.php');//Include the functions file  
-					//Start session
-					//session_start();
+							//If the user has submitted the form
+							if($_POST['submit']){
+								//protect the posted value then store them to variables
+								$username = protect($_POST['username']);
+								$password = protect($_POST['password']);
 
-					//get sessionusername
-					$username=$_SESSION['username'];
+								//Check if the username or password boxes were not filled in
+								if(!$username || !$password){
+									//if not display an error message
+									echo '<div class="alert alert-error span3">You need to fill in a username and a password!</div>';
+								}else{
+									//if the were continue checking
+
+									//select all rows from the table where the username matches the one entered by the user
+									$res = mysql_query("SELECT * FROM `users` WHERE `username` = '".$username."'");
+									$num = mysql_num_rows($res);
+
+									//check if there was not a match
+									if($num == 0){
+										//if not display an error message
+										echo '<div class="alert alert-error span3">Sorry, that username does not exist.</div>';
+									}else{
+										//if there was a match continue checking
+
+										//select all rows where the username and password match the ones submitted by the user
+										$res = mysql_query("SELECT * FROM `users` WHERE `username` = '".$username."' AND `password` = '".$password."'");
+										$num = mysql_num_rows($res);
+
+										//check if there was not a match
+										if($num == 0){
+											//if not display error message
+											echo '<div class="alert alert-error span3">Sorry, that password is incorrect.</div>';
+										}else{
+											//if there was continue checking
+
+											//split all fields fom the correct row into an associative array
+											$row = mysql_fetch_assoc($res);
+
+											//check to see if the user has not activated their account yet
+											if($row['active'] != 1){
+												//if not display error message
+												echo '<div class="alert span3">Sorry, this account has not been activated.</div>';
+											}else{
+												//if they have log them in
+
+												//set the login session storing there id - we use this to see if they are logged in or not
+												$_SESSION['uid'] = $row['id'];
+												//show message
+												echo '<div class="alert alert-success span3">You have successfully logged in!</div>';
+
+												//update the online field to 50 seconds into the future
+												$time = date('U')+50;
+												mysql_query("UPDATE `users` SET `online` = '".$time."' WHERE `id` = '".$_SESSION['uid']."'");
+
+												//redirect them to the usersonline page
+												header('Location: usersOnline.php');
+											}
+										}
+									}
+								}
+							}
+
+							?>
+							<form action="index.php" method="post" class="form-signin span3">
+									<h5 class="pink">Sign In Here</h5>
+									<input type="text" name="username" class="span3" placeholder="Username">
+									<input type="password" name="password" class="span3" placeholder="Password">
+									<p class="reset pink" align="right"><a href="register.php">Sign Up For An Account</a><br/><a href="forgot.php">Forgot Password</a></p>
+									<button class="btn btn-primary" type="submit" name="submit" value="Register" />Sign In</button>
+					<?
+					ob_end_flush();
 					?>
-
-					<form class="form-signin" method="POST" action="<?PHP echo $_SERVER['PHP_SELF']; ?>">
-					<h5 class="form-signin-heading pink">Returning Members? Sign in here!</h5>
-					<input type="text" class="span3" placeholder="Username" name="username" 
-					value="<?PHP echo $username; ?>">
-					<input type="password" name="password" class="span3" placeholder="Password"/>
-					<p class="reset pink" align="right">Not a Member? <a href="register.php">Sign up now!</a> <br/>
-					<a href="password.php">Reset Password?</a></p>
-					<button class="btn btn-primary" type="submit" name="login" value="Login">Sign In</button>
-					</form>
+					<!--End Login Form-->
 				</div>
 			</div><!--/row for intro-->
 		<?php include('includes/footer.php');?>

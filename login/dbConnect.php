3b3d05c2325454aba4412356c758bc0c875f2b38
melanieororke <melanieororke@fullsail.edu>
2013-05-23 14:19:14 -0500

    Cleaning Up File Structure.

--- login/dbConnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$host = 'localhost';
-	$user = 'abandond_easykee';
-	$pass = 'easyKeepr./2013';
-
-	$sqlconnect = mysql_connect($host, $user, $pass) or die ('Error connecting to mysql');
-    mysql_select_db("abandond_easykeepr") or die("Unable to select database");
-?>

6c3ebaa0f7d157a9eace5672bdeacf663108525a
melanieororke <melanieororke@fullsail.edu>
2013-05-23 17:19:23 -0500

    Forgot Password Feature
    Able to retrieve password and activate account.

--- activate.php
+++ activate.php
@@ -1,43 +1,42 @@
-<?php
-//allow sessions to be passed so we can see if the user is logged in
-session_start();
- 
-//connect to the database so we can check, edit, or insert data to our users table
-include('includes/header.php');
- 
-//include out functions file giving us access to the protect() function made earlier
-include "includes/functions.php";
- 
-?>
-		<?php
- 
-		echo md5('other');
-		//get the code that is being checked and protect it before assigning it to a variable
-		$code = protect($_GET['code']);
- 
-		//check if there was no code found
-		if(!$code){
-			//if not display error message
-			echo "<div class='alert alert-error'>Sorry, we have encountered an error.</div>";
-		}else{
-			//other wise continue the check
- 
-			//select all the rows where the accounts are not active
-			$res = mysql_query("SELECT * FROM `users` WHERE `active` = '0'");
- 
-			//loop through this script for each row found not active
-			while($row = mysql_fetch_assoc($res)){
-				//check if the code from the row in the database matches the one from the user
-				if($code == md5($row['username']).$row['rtime']){
-					//if it does then activate there account and display success message
-					$res1 = mysql_query("UPDATE `users` SET `active` = '1' WHERE `id` = '".$row['id']."'");
-					echo "<div class='alert alert-success'>Thank you! Your account is now activated!</div>";
-				}
-			}
-		}
- 
-		?>
-		</div>
-	</body>
-</html>
-<?php ('includes/footer.php');?>
+<?php include('includes/header.php');?>
+			<div class="row"><!--Row for Main Headline-->
+				<div class="span12">
+					<h1 class="pink" align="center">Activate Your Account</h1>
+				</div>
+			</div><!-- /main headline-->
+			<div class="row"><!--Row for HR-->
+				<div class="span12"><hr></div>
+			</div><!-- /row for HR -->
+			<div class="row"><!--Row for intro-->
+				<div class="span12">
+					<?php
+
+					echo md5('other');
+					//get the code that is being checked and protect it before assigning it to a variable
+					$code = protect($_GET['code']);
+
+					//check if there was no code found
+					if(!$code){
+						//if not display error message
+						echo "<div class='alert alert-error'>Sorry, we have encountered an error.</div>";
+					}else{
+						//other wise continue the check
+
+						//select all the rows where the accounts are not active
+						$res = mysql_query("SELECT * FROM `users` WHERE `active` = '0'");
+
+						//loop through this script for each row found not active
+						while($row = mysql_fetch_assoc($res)){
+							//check if the code from the row in the database matches the one from the user
+							if($code == md5($row['username']).$row['rtime']){
+								//if it does then activate there account and display success message
+								$res1 = mysql_query("UPDATE `users` SET `active` = '1' WHERE `id` = '".$row['id']."'");
+								echo "<div class='alert alert-success'>Thank you! Your account is now activated!</div>";
+							}
+						}
+					}
+
+					?>
+				</div>
+			</div><!--/row for intro-->
+		<?php include('includes/footer.php');?>
--- forgot.php
+++ forgot.php
@@ -1,80 +1,71 @@
-<?php
-//allow sessions to be passed so we can see if the user is logged in
-session_start();
- 
-//connect to the database so we can check, edit, or insert data to our users table
-include('includes/header.php');
- 
-//include out functions file giving us access to the protect() function made earlier
-include "includes/functions.php";
- 
-?>
+<?php include('includes/header.php');?>
+			<div class="row"><!--Row for Main Headline-->
+				<div class="span12">
+					<h1 class="pink" align="center">Forgot Your Password?</h1>
+				</div>
+			</div><!-- /main headline-->
+			<div class="row"><!--Row for HR-->
+				<div class="span12"><hr></div>
+			</div><!-- /row for HR -->
+			<div class="row"><!--Row for intro-->
+				<div class="span8">
+					<?php
 
-		<?php
- 
-		//Check to see if the forms submitted
-		if($_POST['submit']){
-			//if it is continue checks
- 
-			//store the posted email to variable after protection
-			$email = protect($_POST['email']);
- 
-			//check if the email box was not filled in
-			if(!$email){
-				//if it wasn't display error message
-				echo "<div class='alert alert-error'>Please enter your e-mail address.</div>";
-			}else{
-				//else continue checking
- 
-				//set the format to check the email against
-				$checkemail = "/^[a-z0-9]+([_\\.-][a-z0-9]+)*@([a-z0-9]+([\.-][a-z0-9]+)*)+\\.[a-z]{2,}$/i";
- 
-				//check if the email doesnt match the required format
-				if(!preg_match($checkemail, $email)){
-					//if not then display error message
-					echo "<div class='alert alert-error'>Sorry, the email address is not valid.</div>";
-				}else{
-					//otherwise continue checking
- 
-					//select all rows from the database where the emails match
-					$res = mysql_query("SELECT * FROM `users` WHERE `email` = '".$email."'");
-					$num = mysql_num_rows($res);
- 
-					//check if the number of row matched is equal to 0
-					if($num == 0){
-						//if it is display error message
-						echo "<div class='alert alert-error'>Sorry, this e-mail address is not registered.</div>";
-					}else{
-						//otherwise complete forgot pass function
- 
-						//split the row into an associative array
-						$row = mysql_fetch_assoc($res);
- 
-						//send email containing their password to their email address
-						mail($email, 'Forgotten Password', "Here is your password: ".$row['password']."\n\nPlease save this message for future reference!", 'From: me@melanieororkedesign.com');
- 
-						//display success message
-						echo "<div class='alert alert-success'>Thank you! An email has been sent to this address.</div>";
+					//Check to see if the forms submitted
+					if($_POST['submit']){
+						//if it is continue checks
+
+						//store the posted email to variable after protection
+						$email = protect($_POST['email']);
+
+						//check if the email box was not filled in
+						if(!$email){
+							//if it wasn't display error message
+							echo "<div class='alert alert-error'>Please enter your e-mail address.</div>";
+						}else{
+							//else continue checking
+
+							//set the format to check the email against
+							$checkemail = "/^[a-z0-9]+([_\\.-][a-z0-9]+)*@([a-z0-9]+([\.-][a-z0-9]+)*)+\\.[a-z]{2,}$/i";
+
+							//check if the email doesnt match the required format
+							if(!preg_match($checkemail, $email)){
+								//if not then display error message
+								echo "<div class='alert alert-error'>Sorry, the email address is not valid.</div>";
+							}else{
+								//otherwise continue checking
+
+								//select all rows from the database where the emails match
+								$res = mysql_query("SELECT * FROM `users` WHERE `email` = '".$email."'");
+								$num = mysql_num_rows($res);
+
+								//check if the number of row matched is equal to 0
+								if($num == 0){
+									//if it is display error message
+									echo "<div class='alert alert-error'>Sorry, this e-mail address is not registered.</div>";
+								}else{
+									//otherwise complete forgot pass function
+
+									//split the row into an associative array
+									$row = mysql_fetch_assoc($res);
+
+									//send email containing their password to their email address
+									mail($email, 'Forgotten Password', "Here is your password: ".$row['password']."\n\nPlease save this message for future reference!", 'From: me@melanieororkedesign.com');
+
+									//display success message
+									echo "<div class='alert alert-success'>Thank you! An email has been sent to this address.</div>";
+								}
+							}
+						}
 					}
-				}
-			}
-		}
- 
-		?>
-			<form action="forgot.php" method="post">
-				<table cellpadding="2" cellspacing="0" border="0">
-					<tr>
-						<td>Email: </td>
-						<td><input type="text" name="email" /></td>
-					</tr>
-					<tr>
-						<td colspan="2" align="center"><button class="btn btn-primary" type="submit" name="submit" value="Send" />Send</button></td>
-					</tr>
-					<tr>
-						<td colspan="2" align="center"><a href="register.php">Register</a> | <a href="login.php">Login</a></a></td>
-					</tr>
-				</table>
-			</form>
-		</div>
-	</body>
-</html>
+
+					?>
+						<form action="forgot.php" method="post">
+							<label>Please enter your email address:</label>
+							<input type="text" name="email" class="span8" />
+							<p class="reset pink" align="right"><a href="register.php">Sign Up For An Account</a><br/><a href="forgot.php">Forgot Password</a></p>
+							<button class="btn btn-primary" type="submit" name="submit" value="Send" />Find My Password!</button>
+						</form>
+						</div>
+			</div><!--/row for intro-->
+		<?php include('includes/footer.php');?>

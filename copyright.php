4c31c0c77c6de5344f3e1c1ffd0ee628bc46beaf
melanieororke <melanieororke@fullsail.edu>
2013-05-24 12:57:04 -0500

    Copyright Information
    Extended copyright notice.

---
+++ copyright.php
@@ -0,0 +1,41 @@
+<?php include('includes/header.php');?>
+			<div class="row"><!--Row for Main Headline-->
+				<div class="span12">
+					<h1 class="pink" align="center">Copyright Information</h1>
+					<p align="center">Copyright &copy; 2013 Melanie O'Rorke | Designed and Developed for Mobile Device Deployment Course at Full Sail University<br/>
+						Easy Keepr is a fictitious site intended for educational purposes only.</p>
+				</div>
+			</div><!-- /main headline-->
+			<div class="row"><!--Row for HR-->
+				<div class="span12"><hr></div>
+			</div><!-- /row for HR -->
+			<div class="row"><!-- Row for Information-->
+			<div class="span6">
+				<h4 class="green">Ownership of Copyright</h4>
+				<p>I, Melanie O'Rorke, own the copyright in:<br/>
+					a) this website; and<br/>
+					b) the material on this website (including and without limitation, the text, computer code, artwork, photographs, images, music, audio material, video material and auto-visual material on this website).</p>
+					<h4 class="green">Credit</h4>
+					<p>This document was created using a <a href="http://www.seqlegal.com/free-legal-documents/copyright-notice">SEQ Legal</a> template.</p>
+					<h4 class="green">Copyright License</h4>
+					<p>I, grant you a world wide, non-exclusive, royalty free, revocable license to:<br/>
+						a) view this website and the material on this website on a computer or mobile device via a web browser;<br/>
+						b) copy and sotre this website and the material on this website in your web browser cache memory; and<br/>
+						c) print pages from this website for your own (personal and non-commericial) use.</p>
+					<p>We do not grant you any other rights in relation to this website or the material on this website. In other words, all rights are reserved.</p>
+					<p>For the avoidance of doubt, you must not adapt, edit, change, transform, publish, republish, distribute, redistribute, broadcast, rebroadcast, or show or play in public this website or the material on this website (in any form or media) without prior written permission.
+			</div>
+			<div class="span6"></div>
+				<h4 class="green">Data Mining</h4>
+				<p>The automated and/or systematic collection of data from this website is prohibited.</p>
+				<h4 class="green">Permissions</h4>
+				<p>You may request permission to use the copyright materials on this website by writing to <a href="mailto:melanie.ororke@gmail.com">melanie.ororke@gmail.com</a>.</p>
+				<h4 class="green">Enforcement of Copyright</h4>
+				<p>I take the protection of my copyright very seriously.</p>
+				<p>If I discover that you have used our copyright materials in contravention of the license above, I may bring legal proceedings against you, seeking monetary damages and/or an injunction to stop you using those materials. You could also be ordered to pay legal costs.</p>
+				<p>If you become aware of any use of my copyright materials that contravenes or may contravene the license above, please report this by email to <a href="mailto:melanie.ororke@gmail.com">melanie.ororke@gmail.com</a>.</p>
+					<h4 class="green">Infringing Material</h4>
+					<p>If you become aware of any material on our website that you believe infringes your or any other person's copyright, please report this by email to <a href="mailto:melanie.ororke@gmail.com">melanie.ororke@gmail.com</a>.</p>
+				</div>
+			</div><!--/row for Information-->
+		<?php include('includes/footer.php');?>

7635985ec7a938b6989729343807b361cdaff410
melanieororke <melanieororke@fullsail.edu>
2013-05-27 15:03:34 -0500

    Members can subscribe to newsletter
    Newsletter form set up

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -20,3 +20,19 @@
         <li><a href="viewEvents.php">View Events</a></li>
       </ul>
     </div><!--/.well -->
+		<!-- Begin MailChimp Signup Form -->
+		<link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+		<style type="text/css">
+			#mc_embed_signup{background:#fff;}
+			/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+			   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+		</style>
+		<div id="mc_embed_signup">
+		<form action="http://melanieororkedesign.us7.list-manage.com/subscribe/post?u=2240081fc84f0c21d61c58684&amp;id=13a10805f5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			<label for="mce-EMAIL"><h5 class="green">Subscribe to Our Mailing List</h5></label>
+			<input type="email" value="" name="EMAIL" id="mce-EMAIL" placeholder="email address" required>
+			<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary"></div>
+		</form>
+		</div>
+
+		<!--End mc_embed_signup-->

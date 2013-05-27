195796a2a4fbc69138135d8c4827414e87251ab8
melanieororke <melanieororke@fullsail.edu>
2013-05-26 21:25:41 -0500

    Abuse Reporting
    User is able to report abuse of the site. Form message is sent via
    email and the user recieves a thank you message.

---
+++ abuse.php
@@ -0,0 +1,36 @@
+<?php include('includes/header.php');?>
+<div class="row"><!--Row for Main Headline-->
+	<div class="span12">
+		<h1 class="pink" align="center">Report Abuse of Easy Keepr</h1>
+	</div>
+</div><!--End Row for Main Headline-->
+<div class="row"><!--Row for HR-->
+	<div class="span12"><hr></div>
+</div><!--End Row for HR-->
+
+<div class="row"><!--Row for Abuse Form-->
+	<div class="span6">
+		<h3 class="green">What is Abuse?</h3>
+		
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. Praesent tempor molestie libero in congue. Morbi nec justo ut libero ornare dignissim a ut erat. Vestibulum malesuada sollicitudin magna non vehicula. Nunc pharetra magna nec odio iaculis pharetra. Nam dignissim lorem lobortis sapien molestie molestie. Duis tempor venenatis dui a varius.</p>
+		
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. Praesent tempor molestie libero in congue. Morbi nec justo ut libero ornare dignissim a ut erat. Vestibulum malesuada sollicitudin magna non vehicula. Nunc pharetra magna nec odio iaculis pharetra. Nam dignissim lorem lobortis sapien molestie molestie. Duis tempor venenatis dui a varius.</p>
+			
+		
+	</div>
+	
+	<div class="span6">
+		<h3 class="green">Report Abuse to Us</h3>
+		<form action="sendAbuse.php" method"post">
+			<label>Your Name:</label>
+			<input type="text" name="name" placeholder="Your Name">
+			<label>Your Email:</label>
+			<input type="text" name="name" placeholder="me@yourdomain.com">
+			<label>Message:</label>
+			<textarea rows="4" name="message" id="message" class="span4"/></textarea><br/>
+			<button class="btn btn-primary" type="submit" name="submit">Report Abuse</button>
+		</form>
+		
+	</div>
+</div><!--End Row for Abuse Form-->
+<?php include('includes/footer.php');?>
---
+++ thanks.php
@@ -0,0 +1,34 @@
+<?php include('includes/header.php');?>
+<div class="row"><!--Row for Main Headline-->
+	<div class="span12">
+		<h1 class="pink" align="center">Thank You!</h1>
+	</div>
+</div><!--End Row for Main Headline-->
+<div class="row"><!--Row for HR-->
+	<div class="span12"><hr></div>
+</div><!--End Row for HR-->
+
+<div class="row"><!--Row for Abuse Form-->
+	<div class="span6">
+		<h3 class="green">Your Message Has Been Sent!</h3>
+		<p>Thank you for contacting us with your abuse concerns. We will reply as soon as possible!</p>
+	</div>
+	
+	<div class="span6">
+		<h3 class="green">Why Sign Up?</h3>
+		<h4 class="grey">Horse Information</h4>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. </p>
+	<hr>
+	<h4 class="grey">Feed Schedules</h4>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. </p>
+	<hr>
+	<h4 class="grey">Health Records</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. </p>
+<hr>
+<h4 class="grey">Show Schedules</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu mi nec nisi semper adipiscing in eget lorem. Proin semper arcu vel sapien ultrices sed sagittis ante tempor. Curabitur ut feugiat lacus. </p>
+
+		
+	</div>
+</div><!--End Row for Abuse Form-->
+<?php include('includes/footer.php');?>

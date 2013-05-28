a6990ca97e777bb3a53a9993dbb71d74a1ec74b7
melanieororke <melanieororke@fullsail.edu>
2013-05-28 14:55:33 -0500

    Removing Back Up File
    Removing Back Up File

--- backup/includes/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-  //This function prevents SQL injection by stripping all tags and adding slashes for quotation marks
-	//and apostrophes.
-	function protect($string){
-		$string = trim(strip_tags(addslashes($string)));
-		return $string;
-	}
-
-?>
